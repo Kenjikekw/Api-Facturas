0ba10e842ff79c301d7cdc22794d78edfd701e0f
GermanCruzPallares <gcp0007@alu.medac.es>
2023-11-06 10:35:44 +0100

    primer controlador getAll

--- Controllers/FacturaController.cs
+++ Controllers/FacturaController.cs
@@ -1,12 +1,31 @@
+using Api.Models;
+using Api.Services;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
-namespace Api.Controllers;
-
-[ApiController]
-[Route("[controller]")]
-public class FacturaController : ControllerBase
+namespace Api.Controllers
 {
-   
-   
-    
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FacturaController : ControllerBase
+    {
+        private readonly IFacturasService _facturasService;
+
+        public FacturaController(IFacturasService facturasService)
+        {
+            _facturasService = facturasService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Factura>> GetAllFacturas()
+        {
+            var facturas = _facturasService.GetAllFactura();
+            if(!facturas.Any()){
+                return NotFound("No se encontro ninguna factura");
+            }
+            return Ok(facturas);
+        }
+
+
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,29 +1,39 @@
 using Api.Models;
+using Api.Services;
 using Microsoft.EntityFrameworkCore;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddDbContext<DataContext>(options=>
-options.UseSqlServer(builder.Configuration.GetConnectionString("DbContext")));
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddDbContext<DataContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DbContext")));
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddScoped<DbContext>(provider => provider.GetService<DataContext>());
+        builder.Services.AddControllers();
+        builder.Services.AddTransient<IFacturasService, FacturasService>();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
